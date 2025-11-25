e9bc59899244ce5a70f1394f8f1177d331cc8202
Sangpts1307 <phamthesang1307@gmail.com>
2025-11-26 00:52:42 +0700

    Feature List Post

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomePage from '../views/HomePage.vue'
+import SidebarComponent from '@/components/SidebarComponent.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/homepage',
+      name: 'homepage',
+      component: HomePage,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+      path: '/sidebar',
+      name: 'sidebar',
+      component: SidebarComponent
+    }
+    
   ],
 })
 
