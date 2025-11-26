9b4d0f7425850cc07d99f48544ef42da96d461d9
unknown <nguyenkimquang1612@gmail.com>
2025-11-26 14:05:37 +0700

    feature auth

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import vue3GoogleLogin from "vue3-google-login";
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(vue3GoogleLogin, {
+  clientId: "239347645874-itv40jo84430r1q7huhg7ao6aaso9jr7.apps.googleusercontent.com",
+});
 app.use(router)
 
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import Login from '@/components/auth/Login.vue'
+import Signup from '@/components/auth/Signup.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -17,6 +18,23 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: Login,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: Signup,
+
+    }
   ],
 })
 
